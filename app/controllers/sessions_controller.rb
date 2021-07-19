00f05f63621e1ea73e680ece3098dd17ab4bf03f
Casey Fayard <fayardcasey@gmail.com>
2021-07-19 15:06:33 -0400

    Adds log_in method to the sessions helper Incudes sessions helper in the app controller
    Updates the create method within the sessions-controller to use new log_in method
    Sets up redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate([params[:session][:password]])
-      # Log user in and redirect to the user show page
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'    
